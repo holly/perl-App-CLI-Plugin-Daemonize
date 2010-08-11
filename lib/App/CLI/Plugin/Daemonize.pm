fb4b1df2010077c5cf2d7cd4ad6c82820d192568
holly <emperor.kurt@gmail.com>
2010-08-11 11:14:19 +0900

    initial commit

---
+++ lib/App/CLI/Plugin/Daemonize.pm
@@ -0,0 +1,32 @@
+package App::CLI::Plugin::Daemonize;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::CLI::Plugin::Daemonize -
+
+=head1 SYNOPSIS
+
+  use App::CLI::Plugin::Daemonize;
+
+=head1 DESCRIPTION
+
+App::CLI::Plugin::Daemonize is
+
+=head1 AUTHOR
+
+Akira Horimoto E<lt>emperor.kurt@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
